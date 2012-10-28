56d6b1d96ec328929c5597e0933c8a2f420982da
Jahangir Zinedine <jzinedine@gmail.com>
2012-10-28 17:50:07 +0330

    assets are fine now esp javascript part, date trasnformation now will be called recursively on objects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,19 @@
 module ApplicationHelper
     
   def to_gregorian params
-    params.each {|a, f|
-      if f.is_a? Hash 
-        f.each { |key, value|
-          if value.is_a?(String) and /13\d{2}\/\d{1,2}\/\d{1,2}/ =~ value
-            year, month, day = /(13\d{2})\/(\d{1,2})\/(\d{1,2})/.match(value).captures
-            jdate = JalaliDate.new(year.to_i, month.to_i, day.to_i)
-            params[a][key]= jdate.to_gregorian.to_s
-          end
-        }  
-      end
-    }
+    params.each do |a, f|
+      return if not f.is_a? Hash 
+      f.each do |key, value|
+        if value.is_a?(String) and /13\d{2}\/\d{1,2}\/\d{1,2}/ =~ value
+          year, month, day = /(13\d{2})\/(\d{1,2})\/(\d{1,2})/.match(value).captures
+          jdate = JalaliDate.new(year.to_i, month.to_i, day.to_i)
+          params[a][key]= jdate.to_gregorian.to_s
+        elsif value.is_a? Hash
+          debugger
+          to_gregorian value
+        end
+      end  
+    end
   end
   
   def to_jalali arg
@@ -19,6 +21,9 @@ module ApplicationHelper
       if value.is_a? Date
         jdate = JalaliDate.new(value)
         arg[name] = jdate.to_s
+      elsif value.is_a? ActiveRecord::Base
+        debugger
+        to_jalali value
       end
     end
     arg
